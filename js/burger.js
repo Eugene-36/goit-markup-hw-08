34de8f94a5b0efcba6dc26550c0959ea6720ba6a
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2020-09-07 00:12:40 +0300

    Добавляет новые данные для студия

---
+++ js/burger.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-modal]'),
+      closeModalBtn: document.querySelector('[data-modal-close-button]'),
+      modal: document.querySelector('[data-menu]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-cover');
+    }
+  })();
